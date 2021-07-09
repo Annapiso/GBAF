fa41920e9e72eae8b474d8465420e39b93b2c7a5
Anna Rajaomazava <arilovamahefatiana_zo_anna@yahoo.com>
2021-07-09 11:10:47 +0200

    J'ai fait en sorte que le changement de mot de passe se fait uniquement quand l'utilisateur est déconnecté

--- connexion.php
+++ connexion.php
@@ -19,59 +19,59 @@
             <?php
             //session_start();
             include("connexionbdd.php");
-            if(isset($_SESSION["success"])){
-                echo '<div class="alert alert-success">' . $_SESSION["success"] . '</div>';
-            }
-            else{
-               // echo "Veuillez indiquer vos informations de connexion.";
-            }
-            if(isset($_POST['boutonconnexion'])) //le bouton est appuyé
-                    {
-                    $pseudo=$_POST['pseudo'];
-                    $motdepasse=$_POST['motdepasse'];
-                    if ($pseudo!=" " && $motdepasse!="") //les infos ne sont pas vides
-                    {
-                        
-                        try {
-                            $sql="SELECT * FROM account WHERE username=:username";
-                            $sql=$bdd->prepare($sql);
-                            $sql->execute(array(':username'=>$pseudo));
-                            $row=$sql->FETCH(PDO::FETCH_ASSOC);
-                            $password=$row['password'];
-                            $verifPassword=password_verify($motdepasse,$password);
-                            $count=$sql->rowcount();
-                            
-                            if ($verifPassword==TRUE && $count==1){ //Mot de passe correct et il existe une seule ligne
-                            $_SESSION["Id_visiteur"]=$row["id_user"];
-                            $_SESSION["Nom_visiteur"]=$row["nom"];
-                            $_SESSION["prenom_visiteur"]=$row["prenom"];
-                            echo '<div class="alert alert-success">'. "Vous allez être redirigé vers la page que vous avez demandé."."</div>";
-                            header("refresh:2; index.php");
-                            }            
-                            else{
-                                echo 'Mot de passe incorrect.';
-                            }
-                       	
-                       
-                        } 
-                        catch (Exception $e)  
-                        {                        
-                        echo "Erreur de connexion. Veuillez indiquer des informations correctes.";//.$e->getMessage();
-                           // goto FORMULAIRE;
-                            }
-                        	
-                        
-                    }
-                    else //un des infos est vide
-                    {
-                    	echo 'Veuillez indiquer vos informations de connexion.';
-                    }
+            if(isset($_SESSION['Nom_visiteur']) && isset($_SESSION["prenom_visiteur"])){// si on est déjà connecté
+                header("location:index.php");
+            }else{
+                if(isset($_SESSION["success"])){
+                    echo '<div class="alert alert-success">' . $_SESSION["success"] . '</div>';
+                }else{
+                   
                 }
-                else  //le bouton n'est pas appuyé.           
-            	{
-            		echo 'Veuillez indiquer vos informations de connexion!';
-               
-           		 }
+                if(isset($_POST['boutonconnexion'])){ //le bouton est appuyé
+                        $pseudo=$_POST['pseudo'];
+                        $motdepasse=$_POST['motdepasse'];
+                        if ($pseudo!=" " && $motdepasse!=""){ //les infos ne sont pas vides
+                        
+                            
+                            try {
+                                $sql="SELECT * FROM account WHERE username=:username";
+                                $sql=$bdd->prepare($sql);
+                                $sql->execute(array(':username'=>$pseudo));
+                                $row=$sql->FETCH(PDO::FETCH_ASSOC);
+                                $password=$row['password'];
+                                $verifPassword=password_verify($motdepasse,$password);
+                                $count=$sql->rowcount();
+                                
+                                if ($verifPassword==TRUE && $count==1){ //Mot de passe correct et il existe une seule ligne
+                                $_SESSION["Id_visiteur"]=$row["id_user"];
+                                $_SESSION["Nom_visiteur"]=$row["nom"];
+                                $_SESSION["prenom_visiteur"]=$row["prenom"];
+                                echo '<div class="alert alert-success">'. "Vous allez être redirigé vers la page que vous avez demandé."."</div>";
+                                header("refresh:2; index.php");
+                                }else{                                
+                                    echo '<div class="alert alert-failure">'.'Mot de passe incorrect.'."</div>";
+                                }
+                           	
+                           
+                            } catch (Exception $e){                           
+                                                    
+                            echo '<div class="alert alert-failure">'."Erreur de connexion. Veuillez indiquer des informations correctes."."</div>";
+                            $e->getMessage();
+                            }
+                            	
+                            
+                        }
+                        else //un des infos est vide
+                        {
+                        	echo 'Veuillez indiquer vos informations de connexion.';
+                        }
+                    }
+                    else  //le bouton n'est pas appuyé.           
+                	{
+                		echo 'Veuillez indiquer vos informations de connexion!';
+                   
+               		 }
+                }
            
            FORMULAIRE:
            ?>
