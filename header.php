e06d53108629735cb975b84b5ddac146d90897e0
Anna Rajaomazava <arilovamahefatiana_zo_anna@yahoo.com>
2021-07-18 23:13:15 +0200

    nettoyage code header

--- header.php
+++ header.php
@@ -8,43 +8,35 @@
     </head>
     <body>
 
-<div id="logo">
- 
-    <img src="image/logo.png"/>
-</div>
+        <div id="logo"> <!--logo gbaf-->
+            <a href="index.php"> <img src="image/logo.png"/></a>
+        </div>
 
-<div class="connexion">
-
-<?php
-    session_start();
-    include("connexionbdd.php");
-       
-        if(isset($_SESSION['Nom_visiteur']) && isset($_SESSION["prenom_visiteur"])){
-            ?>
-            <ul id="menu">
-           
-            <?php
-            
-            echo "<div class=\"compte\">"."<img src=\"image/compte.png\">"." "."<div class=\"utilisateur\">"."<h1>".htmlspecialchars($_SESSION["Nom_visiteur"])." ".htmlspecialchars($_SESSION["prenom_visiteur"])."</h1>"."</div>"."</div>";
-            ?></a>
-                <li class="sousMenu"><ul>
-
-            <?php
-                echo "<p>"."<a href=\"deconnexion.php\">Déconnexion</a>"."</p>";?></a></ul><ul><?php
-                echo "<p>"."<a href=\"modifID.php\">Paramètres du compte</a>"."</p>";?></a></ul></li></ul>
-               
-            
-                       
-         
+        <div class="connexion">
         <?php
+        session_start(); //initialisation de la session
+        include("connexionbdd.php");
+        if(isset($_SESSION['Nom_visiteur']) && isset($_SESSION["prenom_visiteur"])){ // si l'utilisateur est déjà connecté
+            ?>
+            <ul id="menu">           
+                <?php
+            
+                 echo "<div class=\"compte\">"."<img src=\"image/compte.png\">"." "."<div class=\"utilisateur\">"."<h1>".htmlspecialchars($_SESSION["Nom_visiteur"])." ".htmlspecialchars($_SESSION["prenom_visiteur"])."</h1>"."</div>"."</div>";
+                ?>
+                <li class="sousMenu"><ul>
+                <?php
+                echo "<p>"."<a href=\"deconnexion.php\">Déconnexion</a>"."</p>";?></a></ul><ul><?php
+                echo "<p>"."<a href=\"modifID.php\">Paramètres du compte</a>"."</p>";
+                ?></a></ul></li></ul>         
+                 <?php
         }
         else
         {
 
         }
         ?>
-</div>
+        </div>
 
 
-</body>
+    </body>
 </html>
